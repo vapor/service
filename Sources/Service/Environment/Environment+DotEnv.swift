c4d8148bbd22de42fdec1a84f58552913d52388a
Anthony Castelli <neueanthony@me.com>
2018-05-14 10:49:51 -0700

    environment: Give each funtion cleaer names, as well as more extensive documentation

--- Sources/Service/Environment/Environment+DotEnv.swift
+++ Sources/Service/Environment/Environment+DotEnv.swift
@@ -8,30 +8,40 @@ import Core
 
 extension Environment {
     /// Loads the ".env" and the ".env.environment" files if they exist
-    public func loadEnvironment() {
+    /// Both a generic .env file and an environment specific file are loaded.
+    /// This allows you to set custom credentials to specific environments such as
+    /// production, testing, and development.
+    /// If you use custom environments, those will also be loaded.
+    public func load() {
         // Load the default one
-        self.load(envFile: ".env")
+        self.load(environment: ".env")
         
         // Load any environment specific ones next
-        self.load(envFile: ".env.\(self.name)")
+        self.load(environment: ".env.\(self.name)")
     }
     
     /// Loads the specific env file and sets the appropiate environment variables
-    public func load(envFile filename: String) {
+    /// Parse the contents of the .env file into a readable key/value
+    /// - parameter environment: The filename of the .env file to load. i.e. `.env`.
+    /// Note: The .env files must be loaced in the root directory.
+    public func load(environment fileName: String) {
         // Create the file path
-        let url = URL(fileURLWithPath: DirectoryConfig.detect().workDir).appendingPathComponent(filename)
+        let url = URL(fileURLWithPath: DirectoryConfig.detect().workDir).appendingPathComponent(fileName)
         
         // Read the contents of the file
-        guard let contents = try? String(contentsOf: url, encoding: .utf8) else { return }
+        guard let environmentVariables = try? String(contentsOf: url, encoding: .utf8) else { return }
         
         // Loop through all the key/values and set the environment variables
-        self.parse(with: contents).forEach { (key, value) in
+        self.parse(content: environmentVariables).forEach { (key, value) in
             setenv(key, value, 1)
         }
     }
     
     /// Parse the contents of the .env file into a readable key/value
-    func parse(with content: String) -> [String : String] {
+    /// - parameter content: The .env string. This can be multiples lines. i.e. `"USERNAME="Vapor"`
+    /// - returns: Returns a Dictionary of Key/Value strings.
+    /// NOTE: The content must be formatted in a Key=Value format. 
+    func parse(content: String) -> [String : String] {
         let pattern = "(\\w+)=(?:\"([:<>;~=\\[\\]\\w\\s/?(\\\\\")!@#$%^&*(){},`'-\\.\\+\\|]+)\"|([:<>;~=\\[\\]\\w/?!@#$%^&*(){},`'-\\.\\+\\|]+)[\\s\\n\\r]*)"
         guard let regex = try? NSRegularExpression(pattern: pattern) else { return [:] }
         let matches = regex.matches(in: content, options: [], range: NSRange(location: 0, length: content.count))
--- Tests/ServiceTests/EnvironmentTests.swift
+++ Tests/ServiceTests/EnvironmentTests.swift
@@ -5,7 +5,7 @@ class EnvironmentTests: XCTestCase {
     let env = Environment(name: "dotenv", isRelease: false)
     
     func testMultipleLines() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO=someValue
             BAR=theBarValue
             CAR=theCarValue
@@ -16,49 +16,49 @@ class EnvironmentTests: XCTestCase {
     }
     
     func testUnquotedValue() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO=someValue
         """)
         XCTAssertEqual(parsedData["FOO"], "someValue")
     }
     
     func testUnquotedWhitespaceSeparatedValue() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO=some crazy phrase
         """)
         XCTAssertEqual(parsedData["FOO"], "some")
     }
     
     func testQuotedValue() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO="someValue"
         """)
         XCTAssertEqual(parsedData["FOO"], "someValue")
     }
     
     func testQuotedWhitespaceSeparatedValue() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO="some crazy phrase"
         """)
         XCTAssertEqual(parsedData["FOO"], "some crazy phrase")
     }
     
     func testInlineComment() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO=someValueWithAn # Inline Comment
         """)
         XCTAssertEqual(parsedData["FOO"], "someValueWithAn")
     }
     
     func testQuotedValueWithComment() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO="some Value With An" # Inline Comment
         """)
         XCTAssertEqual(parsedData["FOO"], "some Value With An")
     }
     
     func testValueWithSymbols() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO=a!b@c#d$e%f^g&h*j(k)l{m}.,'-+|
         """)
         XCTAssertEqual(parsedData["FOO"], "a!b@c#d$e%f^g&h*j(k)l{m}.,'-+|")
@@ -66,21 +66,21 @@ class EnvironmentTests: XCTestCase {
     }
     
     func test1PasswordGeneratedPasswordWith10Symbols() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO=6#}U2/Ri8#f@AaDJ,VYxDG(?&v+8
         """)
         XCTAssertEqual(parsedData["FOO"], "6#}U2/Ri8#f@AaDJ,VYxDG(?&v+8")
     }
     
     func testEscapedDoubleQuote() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             FOO="some Value \" With An" # Inline Comment
         """)
         XCTAssertEqual(parsedData["FOO"], "some Value \" With An")
     }
     
     func testPostgreSQLUrl() throws {
-        let parsedData = self.env.parse(with: """
+        let parsedData = self.env.parse(content: """
             DATABSE_URL=psql://vapor@localhost:5432/vapor-test
         """)
         XCTAssertEqual(parsedData["DATABSE_URL"], "psql://vapor@localhost:5432/vapor-test")
