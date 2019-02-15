6fd347d331e5f3ad8a8cc7d8432ecda5f4a0967c
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-15 10:59:25 -0600

    Create Environment.Process wrapper for ProcessInfo type

---
+++ Sources/ServiceKit/Environment/Proccess.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+extension Environment {
+    
+    /// The process information of an environment. Wraps `ProcessInto.processInfo`.
+    @dynamicMemberLookup public struct Process {
+        
+        /// The process information of the environment.
+        public let info: ProcessInfo
+        
+        /// Creates a new `Process` wrapper for process information.
+        ///
+        /// - parameter info: The process info that the wrapper accesses. Defaults to `ProcessInto.processInfo`.
+        public init(info: ProcessInfo = .processInfo) {
+            self.info = info
+        }
+        
+        /// Gets a key from the proccess environment
+        ///
+        ///     Environment.process.DATABASE_PORT = 3306
+        ///     Environment.process.DATABASE_PORT // 3306
+        public subscript<T>(dynamicMember member: String) -> T? where T: LosslessStringConvertible {
+            get {
+                guard let raw = self.info.environment[member], let value = T(raw) else {
+                    return nil
+                }
+                
+                return value
+            }
+            set (value) {
+                setenv(member, value?.description, 1)
+            }
+        }
+    }
+}
